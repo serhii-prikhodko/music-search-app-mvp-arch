49af367b8d4e1442000c888d30e5ff85ef096176
Serhiy Prikhodko <serhii.prikhodko@gmail.com>
2020-05-25 15:58:34 +0300

    Initial Commit

---
+++ music-search-app-mvp-arch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  music-search-app-mvp-arch
+//
+//  Created by Serhiy Prikhodko on 5/25/20.
+//  Copyright © 2020 Serhiy Prikhodko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
