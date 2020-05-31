ba794b674b2c7c13b9bce7eaf3333dc405646d47
Serhiy Prikhodko <serhii.prikhodko@gmail.com>
2020-05-31 20:01:38 +0300

    - update module structure; - show search results on table view;

---
+++ music-search-app-mvp-arch/ModuleBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  ModuleBuilder.swift
+//  music-search-app-mvp-arch
+//
+//  Created by Serhiy Prikhodko on 5/31/20.
+//  Copyright © 2020 Serhiy Prikhodko. All rights reserved.
+//
+
+import Foundation
